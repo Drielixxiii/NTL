6fd798d87d3567362312fe03c965ec58e966313d
Drielixxiii <drieli.freitas286@gmail.com>
2021-04-28 11:10:46 -0300

    commit

--- js/app.js
+++ js/app.js
@@ -389,3 +389,26 @@ window.onload = function onLoad() {
 
   circle.animate(0); //1 ou 2
 };
+
+$("#recipeCarousel").carousel({
+  interval: 10000,
+});
+
+$(".carousel .carousel-item").each(function () {
+  var minPerSlide = 3;
+  var next = $(this).next();
+  if (!next.length) {
+    next = $(this).siblings(":first");
+  }
+  next.children(":first-child").clone().appendTo($(this));
+
+  for (var i = 0; i < minPerSlide; i++) {
+    next = next.next();
+    if (!next.length) {
+      next = $(this).siblings(":first");
+    }
+
+    next.children(":first-child").clone().appendTo($(this));
+  }
+});
+
