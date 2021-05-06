aadaf8798f3cff38c2398b5a49991ced992b342a
Drielixxiii <drieli.freitas286@gmail.com>
2021-05-06 11:50:47 -0300

    commit

--- js/app2.js
+++ js/app2.js
@@ -273,3 +273,15 @@ function disableclick(event) {
     return false;
   }
 }
+$(document).ready(function () {
+  $(document).keydown(function (e) {
+    var tecla = window.event.keyCode;
+    if (tecla == 123) {
+      event.returnValue = false;
+    }
+    var ctrl = window.event.ctrlKey;
+    if (ctrl && e.which == 85) {
+      return false;
+    }
+  });
+});
